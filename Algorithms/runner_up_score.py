a42e2b8e4a32e73ec309bf628d2d921b61f8fc1e
Aleksander <65794019+Acidixs@users.noreply.github.com>
2021-02-20 22:59:52 +0100

    Delete runner_up_score.py

--- Algorithms/runner_up_score.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    l = list(map(int, input().split()))
-    m = (max(l))
-    
-    while m == max(l):
-        l.remove(max(l))
-    print(max(l))
