a31d3cddac4dffebea792dcf2fe6fdfc64034ce7
Aleksander <Aleksanderbo10@gmail.com>
2021-02-21 18:10:48 +0100

    added solution for 'Apple and Orange'

---
+++ Apple_and_Orange.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    print(sum([1 for x in apples if (x + a) in range(s, t+1)]))
+    print(sum([1 for x in oranges if (x + b) in range(s, t+1)]))
+
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
